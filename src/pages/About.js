95a9185be8462420923690b1045d6f5a8a702024
Sindi <sindidaconceicao@gmail.com>
2021-05-19 09:17:23 -0400

    about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,33 @@
-export default function About() {
-    return (
-      <div>
-        About
-      </div>
-    )
-  }
+import Image from 'images/image.png'
+import styles from 'styles/about.module.css'
+
+export default function about() {
+  return (
+    <>
+        <div className='titles'>
+            <h1 className='title'>About me</h1>
+            <h2 className='subtitle'>Quien soy</h2>
+        </div>
+        <div className={'content body'}>
+            <div className={styles.section}>
+                <img src={Image} alt=""/>
+                <p>Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit.
+                </p>
+            </div>
+            <div className={styles.section}>
+                <img src={Image} alt=""/>
+                <p>Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit.
+                </p>
+            </div>
+            <div className={styles.section}>
+                <img src={Image} alt=""/>
+                <p>Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit.
+                </p>
+            </div>
+        </div>
+    </>
+  )
+}
