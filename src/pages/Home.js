2135911be194986c7fc05e6e7fe74484a356ad04
Sindi <sindidaconceicao@gmail.com>
2021-05-19 08:59:02 -0400

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Link } from 'react-router-dom'
+import Logo from 'images/logo.png'
+import styles from 'styles/home.module.css'
+
 export default function Home() {
-    return (
+  return (
+    <div className={styles.home}>
+      <img className={styles.logo} src={Logo} alt=""/>
       <div>
-        Home
+        <h1 className='title'>Joel Blanco</h1>
+        <h2 className='subtitle'>Developer Full stack</h2>
       </div>
-    )
-  }
+      <button className={'body-bold '+ styles.button}>
+        <Router>
+          <Link href='/contact'>Contact</Link>
+        </Router>
+      </button>
+    </div>
+  )
+}
